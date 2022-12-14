c50067e9c839524797a74a03f08b0805fc4ee287
Allan Li <allan0499@gmail.com>
2022-12-14 13:44:31 +0200

    Implement first version of test suite for isEmpty

--- test/isEmpty.test.js
+++ test/isEmpty.test.js
@@ -4,4 +4,35 @@ describe('isEmpty function', () => {
   test('should return true if parameter is null ', () => {
     expect(isEmpty(null)).toBe(true)
   })
+  test('should return true if value is not array like', () => {
+    expect(isEmpty(false)).toBe(true)
+    expect(isEmpty(1)).toBe(true)
+  })
+  test('should return true if value is an empty array or object', () => {
+    expect(isEmpty([])).toBe(true)
+    expect(isEmpty({})).toBe(true)
+  })
+  test('should return true if the value is an empty string', () => {
+    expect(isEmpty('')).toBe(true)
+  })
+  test('should return true if the value is an empty buffer', () => {
+    const buffer = Buffer.from('', 'utf-8')
+    expect(isEmpty(buffer)).toBe(true)
+  })
+  test('should return true if the tag of the value is an empty Map', () => {
+    const value = new Map()
+    expect(isEmpty(value)).toBe(true)
+  })
+  test('should return true if the tag of the value is an empty Set', () => {
+    const value = new Set()
+    expect(isEmpty(value)).toBe(true)
+  })
+  test('should return true if the value is an empty Prototype', () => {
+    const objectProto = Object.prototype
+    expect(isEmpty(objectProto)).toBe(true)
+  })
+  test('should return false if the value has its own properties', () => {
+    const value = { a: 1 }
+    expect(isEmpty(value)).toBe(false)
+  })
 })
